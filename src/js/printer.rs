a5df1edf350882e703585a86a46b9b9f47eb3f31
Kevin Barabash <kevinb7@gmail.com>
2022-04-17 23:07:29 -0400

    [chore]: move js_* into js subdir (#12)

--- src/js/printer.rs
+++ src/js/printer.rs
@@ -1,6 +1,6 @@
 use itertools::Itertools;
 
-use super::js_ast::*;
+use super::ast::*;
 
 pub fn print_js(prog: &Program) -> String {
     // TODO: export top-level declarations using `export {foo, bar, baz$0 as baz}` syntax
