be52d847b941454d837dd1738635fa4091697221
Kevin Barabash <kevinb7@gmail.com>
2022-06-12 17:21:17 -0400

    Update the demo to have multiple examples (#113)
    * Update the demo to have multiple examples
    
    * Add scarf emoji as favicon

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,39 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
 
 module.exports = {
-    entry: './demo/index.tsx',
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'index.js',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'index.html',
-        }),
-        new WasmPackPlugin({
-            crateDirectory: path.resolve(__dirname, '.'),
-        }),
+  entry: "./demo/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "index.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html",
+    }),
+    new WasmPackPlugin({
+      crateDirectory: path.resolve(__dirname, "."),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
     ],
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-    mode: 'production', // wasm-pack can't handle source map section 0x20
-    experiments: {
-        asyncWebAssembly: true
-    }
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  mode: "production", // wasm-pack can't handle source map section 0x20
+  experiments: {
+    asyncWebAssembly: true,
+  },
 };
