4b59a2d6db3bf555f2665bd8c5fbe0c55478c325
Kevin Barabash <kevinb7@gmail.com>
2022-07-09 17:24:06 -0400

    implement more of infer() and unify(), start adding tests

--- crates/crochet_infer/src/lib.rs
+++ crates/crochet_infer/src/lib.rs
@@ -1,5 +1,6 @@
 mod constraint_solver;
 mod context;
+pub mod infer;
 mod infer_expr;
 mod infer_lambda;
 mod infer_mem;
@@ -7,9 +8,47 @@ mod infer_pattern;
 mod infer_prog;
 mod infer_type_ann;
 mod substitutable;
-mod util;
 pub mod types;
+mod util;
 
 pub use context::*;
 pub use infer_expr::*;
 pub use infer_prog::*;
+
+#[cfg(test)]
+mod tests {
+    use chumsky::prelude::*;
+    use crochet_parser::*;
+
+    use super::*;
+
+    fn infer(input: &str) -> String {
+        let ctx = Context::default();
+        let expr = expr_parser().parse(input).unwrap();
+        let scheme = infer::infer_expr(&ctx, &expr).unwrap();
+        format!("{scheme}")
+    }
+
+    #[test]
+    fn infer_i_combinator() {
+        assert_eq!(infer("(x) => x"), "<t0>(t0) => t0");
+    }
+
+    #[test]
+    fn infer_k_combinator() {
+        assert_eq!(infer("(x, y) => x"), "<t0, t1>(t0, t1) => t0");
+    }
+
+    #[test]
+    fn infer_s_combinator() {
+        assert_eq!(
+            infer("(f, g, x) => f(x, g(x))"),
+            "<t0, t1, t2>((t0, t1) => t2, (t0) => t1, t0) => t2"
+        );
+    }
+
+    #[test]
+    fn infer_app_of_lam() {
+        assert_eq!(infer("((x) => x)(5)"), "5");
+    }
+}
