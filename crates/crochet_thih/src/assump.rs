1f3adde51e2175fc0a00c3eb0427fff4711db86f
Kevin Barabash <kevinb7@gmail.com>
2022-07-10 23:08:18 -0400

    Remove crochet_thih crate (#140)

--- crates/crochet_thih/src/assump.rs
+++
@@ -1,4 +0,0 @@
-use super::id::*;
-use super::scheme::*;
-
-pub type Assump = (ID, Scheme);
--- crates/crochet_thih/src/scheme.rs
+++
@@ -1,8 +0,0 @@
-use super::r#type::*;
-use super::pred::*;
-
-// data Scheme = Forall [Kind] (Qual Type)
-#[derive(Clone, Debug, Eq, PartialEq)]
-pub enum Scheme {
-    Forall(Qual<Type>)
-}
