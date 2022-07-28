0c8ca9197f0eae08d5c63900f746f78b86aaa255
Kevin Barabash <kevinb7@gmail.com>
2022-07-27 23:33:03 -0400

    Cleanup wasm binding

--- crates/crochet/src/lib.rs
+++ crates/crochet/src/lib.rs
@@ -4,23 +4,6 @@ use crochet_parser::parser;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
-
-    #[wasm_bindgen(js_namespace = console)]
-    fn log(s: &str);
-}
-
-fn greet(msg: &str) {
-    alert(msg);
-}
-
-#[wasm_bindgen]
-pub fn foo() {
-    greet("Hello, world!");
-}
-
-#[wasm_bindgen]
 pub struct CompileResult {
     js: String,
     dts: String,
@@ -28,11 +11,6 @@ pub struct CompileResult {
 
 #[wasm_bindgen]
 impl CompileResult {
-    // #[wasm_bindgen(constructor)]
-    // pub fn new(field: i32) -> Baz {
-    //     Baz { field }
-    // }
-
     #[wasm_bindgen(getter)]
     pub fn js(&self) -> String {
         self.js.to_owned()
@@ -45,14 +23,14 @@ impl CompileResult {
 }
 
 #[wasm_bindgen]
-pub fn compile(input: &str) -> CompileResult {
+pub fn compile(input: &str) -> Result<CompileResult, String> {
     let program = parser().parse(input).unwrap();
 
     let js = crochet_codegen::js::codegen_js(&program);
 
     // TODO: return errors as part of CompileResult
-    let ctx = infer_prog(&program).unwrap();
+    let ctx = infer_prog(&program)?;
     let dts = crochet_codegen::d_ts::codegen_d_ts(&program, &ctx);
 
-    CompileResult { js, dts }
+    Ok(CompileResult { js, dts })
 }
