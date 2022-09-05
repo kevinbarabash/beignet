bd16fcc04f9a734e8b156897659043c927d89d39
Kevin Barabash <kevinb7@gmail.com>
2022-09-05 19:24:10 -0400

    Modify TSX tree-sitter grammar

--- crates/tree_sitter_crochet/grammar.js
+++ crates/tree_sitter_crochet/grammar.js
@@ -2,4 +2,22 @@ const javascript = require("./grammar/tsx");
 
 module.exports = grammar(javascript, {
   name: "crochet",
+
+  // _expressions: ($, previous) => {
+  //   const choices = previous.choices.filter(
+  //     (choice) => choice === $.expression
+  //   );
+  //   return choice(...choices);
+  // },
+
+  rules: {
+    // remove sequence expression and optional flow-style type assertion
+    parenthesized_expression: ($, previous) => {
+      return seq("(", $.expression, ")");
+    },
+    // remove sequence expression
+    _expressions: ($, previous) => {
+      return $.expression;
+    },
+  },
 });
