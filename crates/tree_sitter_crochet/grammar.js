81b7b9819d75951a7eb58b4c349b1d67e122e62c
Kevin Barabash <kevinb7@gmail.com>
2022-09-05 22:22:18 -0400

    make if-else an expression

--- crates/tree_sitter_crochet/grammar.js
+++ crates/tree_sitter_crochet/grammar.js
@@ -4,27 +4,49 @@ module.exports = grammar(tsx, {
   name: "crochet",
 
   rules: {
-    // removes sequence expression and optional flow-style type assertion
+    // Removes sequence expression and optional flow-style type assertion
     parenthesized_expression: ($, previous) => {
       return seq("(", $.expression, ")");
     },
-    // removes sequence expression
+
+    // Removes sequence expression
     _expressions: ($, previous) => {
       return $.expression;
     },
-    // removes ternary expression
+
     expression: ($, previous) => {
+      // Removes ternary expression
       const choices = previous.members.filter(
         (member) => member.name !== "ternary_expression"
       );
+      // Makes if-else an expression
+      choices.push($.if_expression);
       return choice(...choices);
     },
-    // removes with statement
+
+    // Removes with statement
     statement: ($, previous) => {
       const choices = previous.members.filter(
-        (member) => member.name !== "with_statement"
+        (member) =>
+          member.name !== "with_statement" && member.name !== "if_statement"
       );
       return choice(...choices);
+    },
+
+    else_clause: ($, previous) => {
+      // Always require the alternative to be in a block
+      return seq("else", choice($.if_expression, $.statement_block));
+    },
+    if_expression: ($, previous) => {
+      return prec.right(
+        seq(
+          "if",
+          field("condition", $.parenthesized_expression),
+          // Always require the alternative to be in a block
+          field("consequence", $.statement_block),
+          optional(field("alternative", $.else_clause))
+        )
+      );
     },
   },
 });
