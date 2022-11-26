a2b204025b9448742488cc760e3ad4212564d6bb
Kevin Barabash <kevinb7@gmail.com>
2022-11-25 21:17:21 -0500

    tweak the destructuring fixture a bit

--- crates/crochet/tests/pass/destructuring.d.ts
+++ crates/crochet/tests/pass/destructuring.d.ts
@@ -1,10 +1,8 @@
 export declare const a: 1;
-export declare const d: 3;
+export declare const f: 5;
 export declare const rest: {
+    readonly c: 3;
     readonly d: 4;
-    readonly e: 5;
-    readonly f: 6;
-    readonly g: 7;
 };
 export declare const x: 1;
 export declare const z: 3;
--- crates/crochet/tests/pass/destructuring.js
+++ crates/crochet/tests/pass/destructuring.js
@@ -3,12 +3,10 @@ export const [x, $temp_0, z] = [
     2,
     3
 ];
-export const { a , b: $temp_1 , c: d , ...rest } = {
+export const { a , b: $temp_1 , e: f , ...rest } = {
     a: 1,
     b: 2,
     c: 3,
     d: 4,
-    e: 5,
-    f: 6,
-    g: 7
+    e: 5
 };
