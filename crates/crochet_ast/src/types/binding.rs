97f725f29a6aeb373a045dc2f34c4a8829549ff4
Kevin Barabash <kevinb7@gmail.com>
2023-02-17 13:07:58 -0500

    Stop using derivative

--- crates/crochet_ast/src/types/binding.rs
+++ crates/crochet_ast/src/types/binding.rs
@@ -1,11 +1,9 @@
-use derivative::*;
 use std::fmt;
 
 // TODO: have a separate struct for BindingIdents in types so that
 // we don't have to create spans for things that don't need them.
 // TODO: add an `ident` field so that we can have separate spans
-#[derive(Derivative)]
-#[derivative(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct BindingIdent {
     pub name: String,
     pub mutable: bool,
