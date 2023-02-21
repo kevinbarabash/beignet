9efd3f4edb96db8cb61dbab583626cf605c021f1
Kevin Barabash <kevinb7@gmail.com>
2023-02-20 22:11:28 -0500

    Update update.rs to use derived visitor

--- crates/escalier_ast/src/values/class.rs
+++ crates/escalier_ast/src/values/class.rs
@@ -1,37 +1,43 @@
+use derive_visitor::{Drive, DriveMut};
+
 use crate::values::expr::{Block, EFnParam, Expr, Lambda};
 use crate::values::ident::Ident;
 use crate::values::type_ann::{TypeAnn, TypeParam};
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Clone, Debug, Drive, DriveMut, PartialEq, Eq)]
 pub struct Class {
+    #[drive(skip)]
     pub ident: Ident, // Why do have `ident` here an in `ClassDecl`?
     pub body: Vec<ClassMember>,
     pub type_params: Option<Vec<TypeParam>>,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Clone, Debug, Drive, DriveMut, PartialEq, Eq)]
 pub enum ClassMember {
     Constructor(Constructor),
     Method(ClassMethod),
     Prop(ClassProp),
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Clone, Debug, Drive, DriveMut, PartialEq, Eq)]
 pub struct Constructor {
     pub params: Vec<EFnParam>,
     pub body: Block,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Clone, Debug, Drive, DriveMut, PartialEq, Eq)]
 pub struct ClassMethod {
+    #[drive(skip)]
     pub key: Ident,
     pub kind: MethodKind,
     pub lambda: Lambda,
+    #[drive(skip)]
     pub is_static: bool,
+    #[drive(skip)]
     pub is_mutating: bool,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Clone, Debug, Drive, DriveMut, PartialEq, Eq)]
 pub enum MethodKind {
     // TODO: differentiate mutating methods from non-mutating ones
     Method,
@@ -39,12 +45,16 @@ pub enum MethodKind {
     Setter,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Clone, Debug, Drive, DriveMut, PartialEq, Eq)]
 pub struct ClassProp {
+    #[drive(skip)]
     pub key: Ident,
     pub value: Option<Box<Expr>>,
     pub type_ann: Option<Box<TypeAnn>>,
+    #[drive(skip)]
     pub is_static: bool,
+    #[drive(skip)]
     pub is_optional: bool,
+    #[drive(skip)]
     pub is_mutable: bool,
 }
--- crates/escalier_ast/src/values/expr.rs
+++ crates/escalier_ast/src/values/expr.rs
@@ -1,3 +1,5 @@
+use derive_visitor::{Drive, DriveMut};
+
 use crate::types::Type;
 use crate::values::class::Class;
 use crate::values::common::{SourceLocation, Span};
@@ -8,33 +10,43 @@ use crate::values::lit::Lit;
 use crate::values::pattern::{Pattern, PatternKind};
 use crate::values::type_ann::{TypeAnn, TypeParam};
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Clone, Debug, Drive, DriveMut, PartialEq, Eq)]
 pub struct Program {
     pub body: Vec<Statement>,
 }
 
 // TODO: Update Statement to mimic structure of Expr/ExprKind
 // TODO: Update Statement to have an .inferred_type field like Expr
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Clone, Debug, Drive, DriveMut, PartialEq, Eq)]
 pub enum Statement {
     ClassDecl {
+        #[drive(skip)]
         loc: SourceLocation,
+        #[drive(skip)]
         span: Span,
+        #[drive(skip)]
         ident: Ident, // Why do have `ident` here an in `Class`?
         class: Box<Class>,
     },
     VarDecl {
+        #[drive(skip)]
         loc: SourceLocation,
+        #[drive(skip)]
         span: Span,
         pattern: Pattern,
         type_ann: Option<TypeAnn>,
         init: Option<Box<Expr>>,
+        #[drive(skip)]
         declare: bool,
     },
     TypeDecl {
+        #[drive(skip)]
         loc: SourceLocation,
+        #[drive(skip)]
         span: Span,
+        #[drive(skip)]
         declare: bool,
+        #[drive(skip)]
         id: Ident,
         type_ann: TypeAnn,
         type_params: Option<Vec<TypeParam>>,
@@ -43,56 +55,61 @@ pub enum Statement {
     // NOTE: This will never contain a `let` expression since that case is
     // covered by `VarDecl`.
     Expr {
+        #[drive(skip)]
         loc: SourceLocation,
+        #[drive(skip)]
         span: Span,
         expr: Box<Expr>,
     },
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Clone, Debug, Drive, DriveMut, PartialEq, Eq)]
 pub struct Block {
+    #[drive(skip)]
     pub span: Span,
     pub stmts: Vec<Expr>,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Clone, Debug, Drive, DriveMut, PartialEq, Eq)]
 pub struct App {
     pub lam: Box<Expr>,
     pub args: Vec<ExprOrSpread>,
     pub type_args: Option<Vec<TypeAnn>>,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Clone, Debug, Drive, DriveMut, PartialEq, Eq)]
 pub struct Fix {
     pub expr: Box<Expr>,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Clone, Debug, Drive, DriveMut, PartialEq, Eq)]
 pub struct IfElse {
     pub cond: Box<Expr>,
     pub consequent: Block,
     pub alternate: Option<Block>,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Clone, Debug, Drive, DriveMut, PartialEq, Eq)]
 pub struct LetExpr {
     pub pat: Pattern,
     pub expr: Box<Expr>,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Clone, Debug, Drive, DriveMut, PartialEq, Eq)]
 pub struct Lambda {
     pub params: Vec<EFnParam>,
     pub body: Block,
+    #[drive(skip)]
     pub is_async: bool,
     pub return_type: Option<TypeAnn>,
     pub type_params: Option<Vec<TypeParam>>,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Clone, Debug, Drive, DriveMut, PartialEq, Eq)]
 pub struct EFnParam {
     pub pat: Pattern,
     pub type_ann: Option<TypeAnn>,
+    #[drive(skip)]
     pub optional: bool,
 }
 
@@ -105,14 +122,14 @@ impl EFnParam {
     }
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Clone, Debug, Drive, DriveMut, PartialEq, Eq)]
 pub struct Assign {
     pub left: Box<Expr>,
     pub right: Box<Expr>,
     pub op: AssignOp,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Clone, Debug, Drive, DriveMut, PartialEq, Eq)]
 pub enum AssignOp {
     Eq,
     PlusEq,
@@ -123,14 +140,14 @@ pub enum AssignOp {
     // TODO: support bit and logic assign ops
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Clone, Debug, Drive, DriveMut, PartialEq, Eq)]
 pub struct BinaryExpr {
     pub op: BinOp,
     pub left: Box<Expr>,
     pub right: Box<Expr>,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Clone, Debug, Drive, DriveMut, PartialEq, Eq)]
 pub enum BinOp {
     Add,
     Sub,
@@ -144,69 +161,73 @@ pub enum BinOp {
     LtEq,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Clone, Debug, Drive, DriveMut, PartialEq, Eq)]
 pub struct UnaryExpr {
     pub op: UnaryOp,
     pub arg: Box<Expr>,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Clone, Debug, Drive, DriveMut, PartialEq, Eq)]
 pub enum UnaryOp {
     Minus,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Clone, Debug, Drive, DriveMut, PartialEq, Eq)]
 pub struct Obj {
     pub props: Vec<PropOrSpread>,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Clone, Debug, Drive, DriveMut, PartialEq, Eq)]
 pub enum PropOrSpread {
     Spread(SpreadElement),
     Prop(Box<Prop>),
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Clone, Debug, Drive, DriveMut, PartialEq, Eq)]
 pub struct SpreadElement {
     pub expr: Box<Expr>,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Clone, Debug, Drive, DriveMut, PartialEq, Eq)]
 pub enum Prop {
+    #[drive(skip)]
     Shorthand(Ident),
     KeyValue(KeyValueProp),
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Clone, Debug, Drive, DriveMut, PartialEq, Eq)]
 pub struct KeyValueProp {
+    #[drive(skip)]
     pub key: Ident,
     pub value: Box<Expr>,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Clone, Debug, Drive, DriveMut, PartialEq, Eq)]
 pub struct Await {
     pub expr: Box<Expr>,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Clone, Debug, Drive, DriveMut, PartialEq, Eq)]
 pub struct Tuple {
     pub elems: Vec<ExprOrSpread>,
 }
 
 // TODO: make this a enum instead of a struct
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Clone, Debug, Drive, DriveMut, PartialEq, Eq)]
 pub struct ExprOrSpread {
+    #[drive(skip)]
     pub spread: Option<Span>,
     pub expr: Box<Expr>,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Clone, Debug, Drive, DriveMut, PartialEq, Eq)]
 pub struct Member {
     pub obj: Box<Expr>,
+    #[drive(skip)]
     pub prop: MemberProp,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Clone, Debug, PartialEq, Eq)]
 pub enum MemberProp {
     Ident(Ident),
     Computed(ComputedPropName),
@@ -221,81 +242,92 @@ impl MemberProp {
     }
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Clone, Debug, Drive, DriveMut, PartialEq, Eq)]
 pub struct ComputedPropName {
+    #[drive(skip)]
     pub loc: SourceLocation,
+    #[drive(skip)]
     pub span: Span, // includes enclosing []
     pub expr: Box<Expr>,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Clone, Debug, Drive, DriveMut, PartialEq, Eq)]
 pub struct TemplateElem {
+    #[drive(skip)]
     pub loc: SourceLocation,
+    #[drive(skip)]
     pub span: Span,
     pub raw: Lit,
     pub cooked: Lit,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Clone, Debug, Drive, DriveMut, PartialEq, Eq)]
 pub struct TemplateLiteral {
     pub exprs: Vec<Expr>,
     pub quasis: Vec<TemplateElem>,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Clone, Debug, Drive, DriveMut, PartialEq, Eq)]
 pub struct TaggedTemplateLiteral {
+    #[drive(skip)]
     pub tag: Ident,
     // TODO: figure out how to track the span of the `template` part
     pub template: TemplateLiteral,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Clone, Debug, Drive, DriveMut, PartialEq, Eq)]
 pub struct Match {
     pub expr: Box<Expr>,
     pub arms: Vec<Arm>,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Clone, Debug, Drive, DriveMut, PartialEq, Eq)]
 pub struct Arm {
+    #[drive(skip)]
     pub loc: SourceLocation,
+    #[drive(skip)]
     pub span: Span,
     pub pattern: Pattern,
     pub guard: Option<Expr>,
     pub body: Block,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Clone, Debug, Drive, DriveMut, PartialEq, Eq)]
 pub struct New {
     pub expr: Box<Expr>, // should resolve to an object with a constructor signature
     pub args: Vec<ExprOrSpread>,
     pub type_args: Option<Vec<TypeAnn>>,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Clone, Debug, Drive, DriveMut, PartialEq, Eq)]
 pub struct Regex {
+    #[drive(skip)]
     pub pattern: String,
+    #[drive(skip)]
     pub flags: Option<String>,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Clone, Debug, Drive, DriveMut, PartialEq, Eq)]
 pub struct DoExpr {
     pub body: Block,
 }
 
 // Later on we can make this a real decl.  For now, it's here to ease the move
 // to deferring lambda-ization.
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Clone, Debug, Drive, DriveMut, PartialEq, Eq)]
 pub struct LetDecl {
     pub pattern: Pattern,
+    #[drive(skip)]
     pub type_ann: Option<TypeAnn>,
     pub init: Box<Expr>,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Clone, Debug, Drive, DriveMut, PartialEq, Eq)]
 pub enum ExprKind {
     App(App),
     New(New), // like App but for calling constructors to create a new instance
     Fix(Fix),
+    #[drive(skip)]
     Ident(Ident),
     IfElse(IfElse),
     JSXElement(JSXElement),
@@ -303,6 +335,7 @@ pub enum ExprKind {
     Assign(Assign),
     LetExpr(LetExpr), // should only be used in `if let` expressions
     Lit(Lit),
+    #[drive(skip)]
     Keyword(Keyword),
     BinaryExpr(BinaryExpr),
     UnaryExpr(UnaryExpr),
@@ -320,9 +353,11 @@ pub enum ExprKind {
     LetDecl(LetDecl),
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Clone, Debug, Drive, DriveMut, PartialEq, Eq)]
 pub struct Expr {
+    #[drive(skip)]
     pub loc: SourceLocation,
+    #[drive(skip)]
     pub span: Span,
     pub kind: ExprKind,
     pub inferred_type: Option<Type>,
