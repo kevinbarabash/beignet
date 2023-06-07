6de8aacfce74c0198bee1d9b0efe022831c73153
Kevin Barabash <kevinb7@gmail.com>
2023-06-06 23:42:28 -0400

    parse object, tuple, and references types

---
+++ crates/escalier_parser/src/type_ann.rs
@@ -0,0 +1,42 @@
+// TODO:
+// - keywords: number, string, null, undefined, etc.
+// - literals: 5, "hello", true, etc.
+// - tuples and objects
+// - methods, callables, indexers, mapped types
+// - function types
+// - type references (with type params)
+// - typeof, keyof, etc.
+// - conditional types
+
+use crate::source_location::SourceLocation;
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct ObjectProp {
+    pub name: String,
+    pub optional: bool,
+    pub mutable: bool,
+    pub type_ann: TypeAnn,
+}
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub enum TypeAnnKind {
+    BoolLit(bool),
+    Boolean,
+    NumLit(String),
+    Number,
+    StrLit(String),
+    String,
+    Symbol,
+    Null,
+    Undefined,
+    Object(Vec<ObjectProp>),
+    Tuple(Vec<TypeAnn>),
+    Array(Box<TypeAnn>),
+    TypeRef(String, Option<Vec<TypeAnn>>),
+}
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct TypeAnn {
+    pub kind: TypeAnnKind,
+    pub loc: SourceLocation,
+}
