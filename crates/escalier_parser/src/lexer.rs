e2e95eba52fead263ca35303ef0087deecb4ffc8
Kevin Barabash <kevinb7@gmail.com>
2023-06-22 23:13:50 -0400

    clean things up a bit, add a comment

--- crates/escalier_parser/src/lexer.rs
+++ crates/escalier_parser/src/lexer.rs
@@ -9,18 +9,9 @@ use crate::scanner::Scanner;
 use crate::source_location::*;
 use crate::token::*;
 
-// #[derive(Clone)]
-pub enum LexerMode {
-    Normal,
-    Expression,
-    JSXChildren,
-}
-
-// #[derive(Clone)]
 pub struct Lexer<'a> {
     scanner: Scanner<'a>,
-    brace_count: usize,
-    modes: Vec<LexerMode>,
+    brace_counts: Vec<usize>,
     peeked: Option<Token>,
 }
 
@@ -41,8 +32,7 @@ impl<'a> Lexer<'a> {
     pub fn new(input: &'a str) -> Self {
         Self {
             scanner: Scanner::new(input),
-            brace_count: 0, // we need separate brace counts for each mode
-            modes: vec![LexerMode::Normal],
+            brace_counts: vec![0], // we need separate brace counts for each mode
             peeked: None,
         }
     }
@@ -63,15 +53,6 @@ impl<'a> Lexer<'a> {
                 Some(c) => c,
                 None => return None,
             };
-
-            // TODO: handle closing brace in string interpolations
-            match self.modes.last() {
-                Some(LexerMode::Expression) if character == '}' && self.brace_count == 0 => {
-                    self.scanner.pop();
-                    return None;
-                }
-                _ => (),
-            }
 
             let start = self.scanner.position();
 
@@ -150,11 +131,21 @@ impl<'a> Lexer<'a> {
                 '(' => TokenKind::LeftParen,
                 ')' => TokenKind::RightParen,
                 '{' => {
-                    self.brace_count += 1;
+                    let brace_count = self.brace_counts.last_mut().unwrap();
+                    *brace_count += 1;
                     TokenKind::LeftBrace
                 }
                 '}' => {
-                    self.brace_count -= 1;
+                    let brace_count = self.brace_counts.last_mut().unwrap();
+                    // We've matched already matched all of the braces but have
+                    // encountered an extra closing brace.  We could be in a
+                    // template string or a JSX expression so we return and let
+                    // the caller handle it.
+                    if brace_count == &0 {
+                        self.scanner.pop();
+                        return None;
+                    }
+                    *brace_count -= 1;
                     TokenKind::RightBrace
                 }
                 '[' => TokenKind::LeftBracket,
@@ -420,11 +411,9 @@ impl<'a> Lexer<'a> {
                         });
                         self.scanner.pop();
 
-                        self.modes.push(LexerMode::Expression);
-
+                        self.brace_counts.push(0);
                         exprs.push(parse_expr(self));
-
-                        self.modes.pop();
+                        self.brace_counts.pop();
 
                         string = String::new();
                         string_start = self.scanner.position();
@@ -506,14 +495,6 @@ impl<'a> Lexer<'a> {
             None
         } else {
             children = self.lex_jsx_children();
-            // TODO: parse children
-            // - need to match opening and closing tags
-            // - need to parse text nodes
-            // - need to parse expressions
-            // - need to parse JSX elements
-            // while self.scanner.peek(0) != Some('<') {
-            //     self.scanner.pop();
-            // }
 
             assert_eq!(self.scanner.pop(), Some('<'));
             assert_eq!(self.scanner.pop(), Some('/'));
@@ -571,11 +552,9 @@ impl<'a> Lexer<'a> {
             '{' => {
                 self.scanner.pop();
 
-                self.modes.push(LexerMode::Expression);
-
+                self.brace_counts.push(0);
                 let expr = parse_expr(self);
-
-                self.modes.pop();
+                self.brace_counts.pop();
 
                 JSXAttr {
                     name,
@@ -605,11 +584,9 @@ impl<'a> Lexer<'a> {
                 '{' => {
                     self.scanner.pop();
 
-                    self.modes.push(LexerMode::Expression);
-
+                    self.brace_counts.push(0);
                     let expr = parse_expr(self);
-
-                    self.modes.pop();
+                    self.brace_counts.pop();
 
                     children.push(JSXElementChild::ExprContainer(JSXExprContainer {
                         expr: Box::new(expr),
